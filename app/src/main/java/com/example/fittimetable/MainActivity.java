95c5c9033286e5d678a6843c645d1e7bcceb1a25
alena424 <xtesar36@stud.fit.vutbr.cz>
2019-10-10 23:45:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fittimetable/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fittimetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+        button = (Button) findViewById(R.id.login);
+        LinearLayout myLayout = (LinearLayout) this.findViewById(R.id.loginLayout);
+        getSupportActionBar().hide();
+        myLayout.requestFocus();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+   public void openActivity2(){
+        Intent intent = new Intent(this, TimetableActivity.class);
+        startActivity(intent);
+
+    }
+}
