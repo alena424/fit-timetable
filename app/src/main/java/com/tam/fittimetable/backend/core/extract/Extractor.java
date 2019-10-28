810ebbd9549d2207dbe6e58427a0af6c1745bcc4
Petr Kohout <xkohou14@stud.fit.vutbr.cz>
2019-10-28 17:32:00 +0100

    Extractor oprava na mene dnu v tydnyu
    Signed-off-by: Petr Kohout <xkohou14@stud.fit.vutbr.cz>

--- app/src/main/java/com/tam/fittimetable/backend/core/extract/Extractor.java
+++ app/src/main/java/com/tam/fittimetable/backend/core/extract/Extractor.java
@@ -47,6 +47,10 @@ public class Extractor {
             Element dayEl;
 
             for (int y = 0; y < 5; y++) { //iterate through week days
+                if (elm.select("tr").size() <= rowCounter) {
+                    System.out.println("Konec tabulky " + elm.select("tr") );
+                    continue;
+                }
                 dayEl = elm.select("tr").get(rowCounter);
                 day = dayEl.select("th").first().text(); // select day name // Monday, Tuesday...
                 rowsToAnotherDay = Integer.parseInt(dayEl.select("th").first().attr("rowspan")); // selects day rowspan which indicates how many rows day contains
